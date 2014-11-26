7d56590c44a01b24f9cb7eaf933dabc820d69dfe
David Doan <ddoan17@mit.edu>
2014-11-26 06:31:46 -0500

    ball.js animation

--- public/assets/ball.js
+++ public/assets/ball.js
@@ -2,14 +2,34 @@ $(document).ready(function ($) {
 
     $(window).scroll(function () {
 
-        if ($(this).scrollTop() > 0) {
-            $(".ball").css({
-                "display":"inline",
-            });
+        position = $(this).scrollTop()
 
-        } else if($(this).scrollTop() <= 0) {
+        if (position > 0 && position < 480) {
+
+            $(".ball").css({
+
+                "display":"inline",
+                "top":(56.5 + .08*position +"vh"),
+                "left":(30 + .1*position + "vw")
+            
+            });
+            
+            if ($(this).scrollTop() > 480) {
+
+                $(".ball").css({
+                    
+                    "top":(56.5 + .08*480 + .1*position + "vh"),
+                
+                });    
+            }        
+
+            
+
+        } else if(position <= 0) {
+
             $(".ball").css({
                 "display": "none",
+
             });
         }
     });
