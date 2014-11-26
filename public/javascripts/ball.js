5ab8c3b2c6c1c53319cc2402b516dac4e05e88bc
David Doan <ddoan17@mit.edu>
2014-11-26 06:46:35 -0500

    javascripts folder

---
+++ public/javascripts/ball.js
@@ -0,0 +1,32 @@
+$(document).ready(function ($) {
+
+    $(window).scroll(function () {
+
+        position = $(this).scrollTop()
+
+        if (position > 0 && position < 480) {
+
+            $(".ball").css({
+
+                "display":"inline",
+                "top":(56.5 + .08*position +"vh"),
+                "left":(30 + .1*position + "vw")
+            
+            });
+            
+        } else if(position <= 0) {
+
+            $(".ball").css({
+                "display": "none",
+            });
+            
+        } else if (position > 480) {
+
+                $(".ball").css({
+
+                    "top":(56.5 + .1*position + "vh")
+                
+                });    
+            }        
+    });
+});
