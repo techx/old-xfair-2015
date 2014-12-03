3b57dffa3e308da513e92db6664f28ed976f0c17
Rachel Wang <rswang@mit.edu>
2014-12-02 22:25:39 -0500

    wahhh

--- app.js
+++ app.js
@@ -56,5 +56,10 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+// start the server
+var server = app.listen(app.get('port'), app.get('ipaddress'));
 
 module.exports = app;
