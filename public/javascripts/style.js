7963154b3d7f2f394d6a73fdc66a38f4de5c61d3
jzwang <jzwang@mit.edu>
2014-12-31 19:40:15 -0500

    Mobile skrollr

--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -10,14 +10,26 @@ $(document).ready(function(){
         opacity: 1
     }, 1500);
 
-    var s = skrollr.init({
-        forceHeight: false,
-        constants: {
-            foobar: function() {
-                return $(window).height() - 80;
-            }
+    $(function(){
+        var windowWidth = $(window).width();
+        console.log(windowWidth);
+        if (windowWidth > 760) {
+                var s = skrollr.init({
+                forceHeight: false,
+                constants: {
+                    foobar: function() {
+                        return $(window).height() - 80;
+                    }
+                }
+            });
+        } else {
+            $('section.about .container').css("position", "relative");
+            $('section.about .container .wrapper #aboutcontent').css("width", "80vw");
+            $('section.main .container').css("position", "absolute");
+            $('section.main .triangle-contrast').css("position", "absolute");
         }
     });
+
     $('body').css('height','auto');
 });
 
