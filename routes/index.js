fc16c96d783adc687ebdc2707b51902a199ee7b7
Rachel Wang <rswang@mit.edu>
2015-01-09 01:34:50 -0500

    remove banquet route

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,6 @@ router.get('/booths', function(req, res) {
   res.render('booths', { title: 'xFair 2015 / Booth Layout' });
 });
 
-router.get('/banquet', function(req, res) {
-    res.render('banquet', { title: 'xFair 2015 / Banquet' })
-});
-
 router.get('/portal', function(req, res) {
     res.redirect('http://portal.mit-xfair.org');
 });
