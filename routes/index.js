e353817a7d2a0614aa961bb73a16df60a9bb3911
Rachel Wang <rswang@mit.edu>
2015-03-04 15:33:52 -0500

    Remove dymo download

--- routes/index.js
+++ routes/index.js
@@ -34,9 +34,9 @@ router.get('/banquet', function(req, res) {
     res.status(200).sendFile(__dirname + '/pdf/banquet.pdf');
 });
 
-router.get('/dymo', function(req, res) {
-    res.status(200).sendFile(__dirname + '/techfair-checkin.zip');
-});
+// router.get('/dymo', function(req, res) {
+//     res.status(200).sendFile(__dirname + '/techfair-checkin.zip');
+// });
 
 router.get('/map', function(req, res) {
     res.status(200).sendFile(__dirname + '/pdf/map.pdf');
